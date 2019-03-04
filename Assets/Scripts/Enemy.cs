c3b223381da4b6f0f37d0d8dd2fafae746a75db9
Montekis <Montekis@users.noreply.github.com>
2019-03-04 14:46:18 +0200

    Updated colors

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+    public float jumpVelocity;
+    
+
+
+    private void Start()
+    {
+        target = WayPoints.points[0];
+
+        
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+ 
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World ); // kad speedas butu vienodas, delta time skirtas tam, kad veiktu nors ir ant letai frame turimu kompiuteriu
+        transform.Translate(Vector3.up * jumpVelocity * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+        
+    }
+
+    void GetNextWayPoint()
+    {
+        if (wavepointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = WayPoints.points[wavepointIndex];
+    }
+}
---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+       // GameObject path = GameObject.Find("/Environment/Ground (10)");
+       // path.GetComponent<Renderer>().material.color = Color.red;
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.color = Color.gray;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+       // Color[] colors = {Color.black, Color.white};
+       // System.Random random = new System.Random();
+       // Renderer renderer = GetComponent<Renderer>();
+       // int randomNumber = random.Next(0, 1);
+       // renderer.material.color = colors[randomNumber];
+
+       
+    }
+}
